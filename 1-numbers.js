90401586fa40d537877c7aba89385da150643556
alinagranda <alinagrandacasco@gmail.com>
2022-04-07 19:30:45 +0200

    Mi primer commit

---
+++ 1-numbers.js
@@ -0,0 +1,17 @@
+var denisse = 31;
+var jaume = 10;
+var alina = 41;
+var nereo = 35;
+var sumaEdades = denisse + jaume + alina + nereo;
+if (sumaEdades % 2 == 0) {
+    console.log("La suma de edades es par");
+}
+else {
+    console.log("La suma de edades es IMPAR");
+}
+if (sumaEdades > 100) {
+    console.log("Somos viejos!", sumaEdades);
+}
+else {
+    console.log("Aún tenemos esperanza!");
+}
---
+++ 1-numbers.ts
@@ -0,0 +1,21 @@
+let denisse: number = 31;
+let jaume: number = 10;
+let alina: number = 41;
+let nereo: number = 35;
+
+let sumaEdades: number = denisse + jaume + alina + nereo;
+
+if (sumaEdades % 2 == 0) {
+    console.log("La suma de edades es par");
+}
+else {
+    console.log("La suma de edades es IMPAR");
+}
+
+if (sumaEdades > 100) {
+    console.log("Somos viejos!", sumaEdades);
+}
+else {
+    console.log("Aún tenemos esperanza!");
+}
+
