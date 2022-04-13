36262a7d153ca520acc7d1735fb41018c448e668
alinagranda <alinagrandacasco@gmail.com>
2022-04-13 20:19:16 +0200

    ejercicio vidas noemi juana

--- 1-numbers.js
+++ 1-numbers.js
@@ -2,8 +2,10 @@ var denisse = 31;
 var jaume = 10;
 var alina = 41;
 var nereo = 35;
-var lucia = 9;
+var lucia = 10;
 var noe = 40;
+var juana = 68;
+var noemi = 15;
 var sumaEdades = denisse + jaume + alina + nereo;
 if (sumaEdades % 2 == 0) {
     console.log("La suma de edades es par");
@@ -19,3 +21,12 @@ else {
 }
 var vidas = noe / lucia;
 console.log("Vidas de Lucia en Noe ", vidas);
+var vidasnoemienjuana = juana / noemi;
+console.log("vidas de noemi en juana", vidasnoemienjuana);
+if (vidasnoemienjuana > vidas) {
+    console.log("Vidas de Noemi en Juana son mayores que Lucia en Noe");
+}
+else {
+    console.log("Vidas de Lucia en Noe son mayores que las vidas de Noemi en Juana ");
+}
+8;
--- 1-numbers.ts
+++ 1-numbers.ts
@@ -4,7 +4,8 @@ let alina: number = 41;
 let nereo: number = 35;
 let lucia: number = 10;
 let noe: number = 40;
-
+let juana: number = 68;
+let noemi: number = 15;
 
 let sumaEdades: number = denisse + jaume + alina + nereo;
 
@@ -23,3 +24,44 @@ else {
 }
  let vidas : number = noe / lucia ; 
  console.log("Vidas de Lucia en Noe ",vidas);
+
+ let vidasnoemienjuana : number = juana / noemi ;
+ console.log("vidas de noemi en juana",vidasnoemienjuana) 
+
+ if (vidasnoemienjuana > vidas){
+     console.log("Vidas de Noemi en Juana son mayores que Lucia en Noe");
+ } else {
+     console.log("Vidas de Lucia en Noe son mayores que las vidas de Noemi en Juana ");
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 8
+
+
